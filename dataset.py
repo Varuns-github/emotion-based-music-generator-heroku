6baac794712b641461fe716cb6a9f2b0507a7eff
Varuns-github <varun.or.varun@gmail.com>
2022-08-15 17:57:08 +0100

    Update path

--- dataset.py
+++ dataset.py
@@ -1,38 +1,38 @@
 import random
 
 angry = [
-    "https://github.com/Varuns-github/emotion-based-music-generator/blob/main/PredictedMusic/angry_3_lookback.wav",
     "https://github.com/Varuns-github/emotion-based-music-generator/blob/main/PredictedMusic/angry_5_lookback.wav",
+    "https://github.com/Varuns-github/emotion-based-music-generator/blob/main/PredictedMusic/angry_3_lookback.wav",
 ];
 
 disgust = [
-    "https://github.com/Varuns-github/emotion-based-music-generator/blob/main/PredictedMusic/disgust_3_lookback.wav",
     "https://github.com/Varuns-github/emotion-based-music-generator/blob/main/PredictedMusic/disgust_5_lookback.wav",
+    "https://github.com/Varuns-github/emotion-based-music-generator/blob/main/PredictedMusic/disgust_3_lookback.wav",
 ];
 
 fear = [
-    "https://github.com/Varuns-github/emotion-based-music-generator/blob/main/PredictedMusic/fear_3_lookback.wav",
     "https://github.com/Varuns-github/emotion-based-music-generator/blob/main/PredictedMusic/fear_5_lookback.wav",
+    "https://github.com/Varuns-github/emotion-based-music-generator/blob/main/PredictedMusic/fear_3_lookback.wav",
 ];
 
 happy = [
-    "https://github.com/Varuns-github/emotion-based-music-generator/blob/main/PredictedMusic/happy_3_lookback.wav",
     "https://github.com/Varuns-github/emotion-based-music-generator/blob/main/PredictedMusic/happy_5_lookback.wav",
+    "https://github.com/Varuns-github/emotion-based-music-generator/blob/main/PredictedMusic/happy_3_lookback.wav",
 ]
 
 neutral = [
-    "https://github.com/Varuns-github/emotion-based-music-generator/blob/main/PredictedMusic/neutral_3_lookback.wav",
     "https://github.com/Varuns-github/emotion-based-music-generator/blob/main/PredictedMusic/neutral_5_lookback.wav",
+    "https://github.com/Varuns-github/emotion-based-music-generator/blob/main/PredictedMusic/neutral_3_lookback.wav",
 ]
 
 sad = [
-    "https://github.com/Varuns-github/emotion-based-music-generator/blob/main/PredictedMusic/sad_3_lookback.wav",
     "https://github.com/Varuns-github/emotion-based-music-generator/blob/main/PredictedMusic/sad_5_lookback.wav",
+    "https://github.com/Varuns-github/emotion-based-music-generator/blob/main/PredictedMusic/sad_3_lookback.wav",
 ]
 
 surprise = [
-    "https://github.com/Varuns-github/emotion-based-music-generator/blob/main/PredictedMusic/surprise_3_lookback.wav",
     "https://github.com/Varuns-github/emotion-based-music-generator/blob/main/PredictedMusic/surprise_5_lookback.wav",
+    "https://github.com/Varuns-github/emotion-based-music-generator/blob/main/PredictedMusic/surprise_3_lookback.wav",
 ]
 
 
